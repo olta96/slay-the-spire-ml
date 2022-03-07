05b1ac2dda5cfafa00a9d541ab5d9b8895f1983c
Olof <olof@takman.se>
2022-03-07 11:10:33 +0100

    extracted first data from source

---
+++ source_builder.py
@@ -0,0 +1,27 @@
+import json
+
+from data_source_path import data_source_path
+
+
+
+def read_json_file():
+    with open(data_source_path + "/2020-11-01-00-07#1067.json", "r") as json_file:
+        json_contents = json_file.read()
+        return json.loads(json_contents)
+    
+runs = read_json_file()
+print(len(runs))
+
+result_runs = []
+for run in runs:
+    event = run["event"]
+    if event["character_chosen"] == "IRONCLAD"\
+    and event["ascension_level"] >= 10\
+    and event["floor_reached"] >= 44:
+        result_runs.append(run)
+
+with open("out.json", "w+") as json_file:
+    json_file.write(json.dumps(result_runs, indent=4))
+
+print(len(result_runs))
+
