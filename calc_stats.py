991a79e802a1f7e33d4752125ce19a3ae6487c9c
olof <olof@takman.se>
2022-03-29 00:12:50 +0200

    calc_stats works as expected again.

--- calc_stats.py
+++ calc_stats.py
@@ -10,40 +10,41 @@ card_ids = get_card_ids()
 
 
 
-def get_choices_data():
-    with open("choices.json", "r") as choices_json_file:
-        return json.loads(choices_json_file.read())
+def get_one_hot_encoded_data():
+    with open("one_hot_encoded_data.json", "r") as one_hot_file:
+        return json.loads(one_hot_file.read())
 
-choices_data = get_choices_data()
+one_hot_data = get_one_hot_encoded_data()
 
-card_counts = {}
-for choices in choices_data:
-    for choice in choices["choices"]:
-        if choice not in card_counts:
-            card_counts[choice] = 0
-
-        card_counts[choice] += 1
+card_appearance_counts = {}
+for choice in one_hot_data:
+    for card_id, value in enumerate(choice["inputs"]["available_choices"]):
+        if value:
+            if card_id not in card_appearance_counts:
+                card_appearance_counts[card_id] = 1
+            else:
+                card_appearance_counts[card_id] += 1
 
 chosen_card_counts = {}
-for choices in choices_data:
-    player_choice = choices["player_choice"]
-    if player_choice not in chosen_card_counts:
-        chosen_card_counts[player_choice] = 0
-
-    chosen_card_counts[player_choice] += 1
+for choice in one_hot_data:
+    for card_id, value in enumerate(choice["targets"]):
+        if value:
+            if card_id not in chosen_card_counts:
+                chosen_card_counts[card_id] = 1
+            else:
+                chosen_card_counts[card_id] += 1
 
 # sort
-card_counts = {k: v for k, v in sorted(card_counts.items(), key=lambda item: item[1], reverse=True)}
+card_appearance_counts = {k: v for k, v in sorted(card_appearance_counts.items(), key=lambda item: item[1], reverse=True)}
 chosen_card_counts = {k: v for k, v in sorted(chosen_card_counts.items(), key=lambda item: item[1], reverse=True)}
 
 toWrite = "Card appearances:"
+for card_id in card_appearance_counts:
+    toWrite += f"\n\t{card_ids[card_id]}: {round((card_appearance_counts[card_id] / len(one_hot_data)) * 100, 2)} %"
 
-for card_id in card_counts:
-    toWrite += f"\n\t{card_ids[card_id]}: {round((card_counts[card_id] / len(choices_data)) * 100, 2)} %"
-
-toWrite += "\nCard picked:"
+toWrite += "\n\nCard picked:"
 for card_id in chosen_card_counts:
-    toWrite += f"\n\t{card_ids[card_id]}: {round((chosen_card_counts[card_id] / len(choices_data)) * 100, 2)} %"
+    toWrite += f"\n\t{card_ids[card_id]}: {round((chosen_card_counts[card_id] / len(one_hot_data)) * 100, 2)} %"
 
 with open("stats.txt", "w") as stats_file:
     stats_file.write(toWrite)
