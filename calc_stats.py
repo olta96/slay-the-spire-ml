778961e6e1643d864f1a5007283bff6930c7dab1
olof <olof@takman.se>
2022-04-06 22:29:45 +0200

    Calculates Act distribution.

--- calc_stats.py
+++ calc_stats.py
@@ -17,6 +17,7 @@ def get_one_hot_encoded_data():
 one_hot_data = get_one_hot_encoded_data()
 
 card_appearance_counts = {}
+act_counts = {}
 for choice in one_hot_data:
     for card_id, value in enumerate(choice["inputs"]["available_choices"]):
         if value:
@@ -24,6 +25,12 @@ for choice in one_hot_data:
                 card_appearance_counts[card_id] = 1
             else:
                 card_appearance_counts[card_id] += 1
+    for act_id, value in enumerate(choice["inputs"]["acts"]):
+        if value:
+            if act_id not in act_counts:
+                act_counts[act_id] = 1
+            else:
+                act_counts[act_id] += 1
 
 chosen_card_counts = {}
 for choice in one_hot_data:
@@ -37,6 +44,7 @@ for choice in one_hot_data:
 # sort
 card_appearance_counts = {k: v for k, v in sorted(card_appearance_counts.items(), key=lambda item: item[1], reverse=True)}
 chosen_card_counts = {k: v for k, v in sorted(chosen_card_counts.items(), key=lambda item: item[1], reverse=True)}
+act_counts = {k: v for k, v in sorted(act_counts.items(), key=lambda item: item[1], reverse=True)}
 
 toWrite = "Card appearances:"
 for card_id in card_appearance_counts:
@@ -45,6 +53,10 @@ for card_id in card_appearance_counts:
 toWrite += "\n\nCard picked:"
 for card_id in chosen_card_counts:
     toWrite += f"\n\t{card_ids[card_id]}: {round((chosen_card_counts[card_id] / len(one_hot_data)) * 100, 2)} %"
+
+toWrite += "\n\nAct distribution:"
+for act_id in act_counts:
+    toWrite += f"\n\t{act_id}: {round((act_counts[act_id] / len(one_hot_data)) * 100, 2)} %"
 
 with open("stats.txt", "w") as stats_file:
     stats_file.write(toWrite)
