7ba7d5a82f8a9909b9e4bcf32212820933c5fc7a
olof <olof@takman.se>
2022-04-26 15:56:48 +0200

    Plotter now creates plots for loss and accuracy.

--- Plotter.py
+++ Plotter.py
@@ -1,4 +1,6 @@
-import json
+import json, matplotlib, numpy as np
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
 
 
 
@@ -15,9 +17,10 @@ class Plotter:
     - barchart - amount of legendary cards chosen
     """
     
-    def __init__(self, name):
-        self.name = name
-        self.to_save = []
+    def __init__(self, name = None):
+        if name is not None:
+            self.name = name
+            self.to_save = []
 
     def push_epoch(self, epoch, loss, accuracy):
         self.to_save.append((epoch, loss, accuracy))
@@ -43,3 +46,62 @@ class Plotter:
 
     def plot(self):
         graph_data = self.read_graph_data()
+
+        max_loss = 0
+        max_accuracy = 0
+        y_losses = []
+        y_accuracies = []
+        x_epochs = []
+        labels = []
+
+        for dp in graph_data:
+            x_epoch = []
+            y_loss = []
+            y_accuracy = []
+
+            for row in dp["data"]:
+                x_epoch.append(row[0] + 1)
+                y_loss.append(row[1])
+                y_accuracy.append(row[2])
+                max_loss = max(max_loss, row[1])
+                max_accuracy = max(max_accuracy, row[2])
+
+            y_losses.append(y_loss)
+            y_accuracies.append(y_accuracy)
+            x_epochs.append(x_epoch)
+
+            labels.append(dp["name"])
+
+        figure_loss, axes_loss = plt.subplots()
+
+        axes_loss.set_xlabel("Epoch")
+        axes_loss.set_ylabel("Loss")
+        axes_loss.set_title("Loss")
+        axes_loss.set_xticks(np.arange(0, len(x_epochs[0]) + 1, 1))
+        for i in range(len(labels)):
+            axes_loss.plot(x_epochs[i], y_losses[i], label=f"Case{i+1}")
+
+        axes_loss.legend()
+
+        # save plot
+        figure_loss.savefig("loss.png")
+
+        figure_accuracy, axes_accuracy = plt.subplots()
+
+        axes_accuracy.set_xlabel("Epoch")
+        axes_accuracy.set_ylabel("Accuracy %")
+        axes_accuracy.set_title("Accuracy")
+        axes_accuracy.set_xticks(np.arange(0, len(x_epochs[0]) + 1, 1))
+        axes_accuracy.set_ylim(0, 100)
+        for i in range(len(labels)):
+            axes_accuracy.plot(x_epochs[i], y_accuracies[i], label=f"Case{i+1}")
+
+        axes_accuracy.legend()
+
+        # save plot
+        figure_accuracy.savefig("accuracy.png")
+
+
+if __name__ == "__main__":
+    plotter = Plotter()
+    plotter.plot()
